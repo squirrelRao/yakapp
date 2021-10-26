675aa1c876875c8d74ae12cd97fb7490a8f27644
squirrelRao <hqraop@163.com>
2021-10-26 12:33:33 +0800

    add query assets api and related logic

--- lib/page/assets.dart
+++ lib/page/assets.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:yakapp/util/configs.dart';
+import 'package:yakapp/util/net_util.dart';
 
 class AssetsPage extends StatefulWidget{
 
@@ -9,10 +12,34 @@ class AssetsPage extends StatefulWidget{
 
 class AssetsState extends State<AssetsPage>{
 
-   List<Widget> assets = <Widget>[
-     AssetsSummary(),
-     AssetItem()
-   ];
+  dynamic datas;
+
+  void getUserAssets() async {
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? uid = prefs.getString("uid");
+    if(uid == ""){
+      return;
+    }
+
+    ((NetClient()).post(Configs.getAssetsApi, {"user_id":uid}, (data){
+
+            setState(() {
+                datas = data;
+            });
+    }));
+
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance!.addPostFrameCallback( (timestamp)=> getUserAssets());
+
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +49,41 @@ class AssetsState extends State<AssetsPage>{
               title: const Text('资产'),
               backgroundColor: Colors.teal,
             ),
-            body: ListView(
-              scrollDirection: Axis.vertical,
-              children: this.assets,
-            )
+            body: ListView.builder(
+                itemCount: datas.length,
+                itemBuilder: (BuildContext context,int index) {
+
+
+                    if(index == 0){
+
+                      var total_ror = datas["accumulates_ror"];
+                      var total_return = datas["accumulates_ror_return"];
+                      var total_free = datas["accumulates_ror_free"];
+                      var price_unit = datas["price_unit"];
+                      var ror_duration = datas["ror_duration"];
+                      return AssetsSummary(total_ror, total_return, total_free, price_unit, ror_duration);
+
+                    }else{
+
+                      dynamic data = datas[index];
+
+                      var name = data["asset"];
+                      var free = data["free"];
+                      var price = data["price"];
+                      var ror = data["ror"];
+                      var ror_return = data["return"];
+                      var target_ror=data["target_ror"];
+                      var lowest_ror=data["lowest_ror"];
+                      var price_unit = data["price_unit"];
+                      return AssetItem(name,free,price,ror,ror_return,target_ror,lowest_ror,price_unit);
+
+
+                    }
+
+
+
+            })
+
     );
   }
 }
@@ -33,12 +91,35 @@ class AssetsState extends State<AssetsPage>{
 
 class AssetItem extends StatefulWidget{
 
+  AssetItem(name,free,price,ror,ror_return,target_ror,lowest_ror,price_unit);
+
+
+  var name;
+  var free;
+  var price;
+  var ror;
+  var ror_return;
+  var target_ror="-";
+  var lowest_ror="-";
+  var price_unit;
+
 
   @override
-  State createState()=> AssetState();
+  State createState()=> AssetState(name,free,price,ror,ror_return,target_ror,lowest_ror,price_unit);
 }
 
 class AssetState extends State<AssetItem>{
+
+  AssetState(name,free,price,ror,ror_return,target_ror,lowest_ror,price_unit);
+  
+  var name;
+  var free;
+  var price;
+  var ror;
+  var ror_return;
+  var target_ror="-";
+  var lowest_ror="-";
+  var price_unit;
 
   @override
   Widget build(BuildContext context) {
@@ -53,21 +134,21 @@ class AssetState extends State<AssetItem>{
             children: [
               Row(
                 children: [
-                  Expanded(child: AssetData("BTC","100"))
-
+                  Expanded(child: AssetData("币种:",name))
                   ],
               ),
               Row(
                 children: [
-                  Expanded(child:AssetData("持仓","100")),
-                  Expanded(child:AssetData("最新价格","100"))
+                  Expanded(child:AssetData("持仓:",free)),
+                  Expanded(child:AssetData("价格",price+price_unit)),
+                  Expanded(child:AssetData("收益:",ror_return+price_unit))
                 ],
               ),
               Row(
                 children: [
-                  Expanded(child:AssetData("持有收益","100%")),
-                  Expanded(child:AssetData("目标收益","200%")),
-                  Expanded(child:AssetData("止损收益","60%")),
+                  Expanded(child:AssetData("收益率:",ror)),
+                  Expanded(child:AssetData("目标收益:",target_ror)),
+                  Expanded(child:AssetData("止损收益:",lowest_ror))
                 ],
               )
 
@@ -129,11 +210,28 @@ class AssetDataState extends State<AssetData>{
 
 class AssetsSummary extends StatefulWidget{
 
+  AssetsSummary(this.total_ror,this.total_return,this.total_free,this.price_unit,this.ror_duration);
+
+  var total_ror;
+  var total_return;
+  var total_free;
+  var price_unit;
+  var ror_duration;
+
   @override
-  State<StatefulWidget> createState() => AssetsSummaryState();
+  State<StatefulWidget> createState() => AssetsSummaryState(total_ror,total_return,total_free,ror_duration,price_unit);
 }
 
 class AssetsSummaryState extends State<AssetsSummary>{
+
+  AssetsSummaryState(this.total_ror,this.total_return,this.total_free,ror_duration,price_unit);
+
+  var total_ror;
+  var total_return;
+  var total_free;
+  var ror_duration;
+  var price_unit;
+
 
   @override
   Widget build(BuildContext context) {
@@ -148,38 +246,14 @@ class AssetsSummaryState extends State<AssetsSummary>{
           child:Row(
 
             children: [
-                Expanded(
-                  child: Container(
-                    child: Text(
-                      "总资产(UDST)",
-                      style: TextStyle(fontSize: 15.0),
-                    ),
-                  )
-                  ),
-                Expanded(
-                    child:Container(
-                          child: Text(
-                            "100",
-                            style: TextStyle(fontSize: 15.0),
-                          ),
-                        )
-                ),
-                Expanded(
-                    child: Container(
-                      child: Text(
-                        "总收益",
-                        style: TextStyle(fontSize: 15.0),
-                      ),
-                    )
-                ),
-                Expanded(
-                    child:Container(
-                      child: Text(
-                        "100%",
-                        style: TextStyle(fontSize: 15.0),
-                      ),
-                    )
-                ),
+              Row(
+                children: [
+                  Expanded(child: AssetData("总资产:",total_ror+price_unit)),
+                  Expanded(child: AssetData("累计收益:",total_return+price_unit)),
+                  Expanded(child: AssetData("累计收益率:",total_free)),
+                  Expanded(child: AssetData("收益天数:",ror_duration))
+                ],
+              ),
               ],
             )
 
--- lib/util/configs.dart
+++ lib/util/configs.dart
@@ -8,12 +8,12 @@ class Configs {
   static const String loginApi = baseUrl +"/v1/login";
   static const String getBindInfo = baseUrl + "/v1/bind/info";
   static const String bindApi = baseUrl +"/v1/bind_exchange";
-  static const String modifyPasswordApi = baseUrl +"/v1/regist";
-  static const String getAssetsApi = baseUrl +"/v1/regist";
-  static const String getOrderApi = baseUrl +"/v1/regist";
+  static const String getAssetsApi = baseUrl +"/v1/assets/detail";
   static const String updateAssetRorConfigApi = baseUrl +"/v1/regist";
+  static const String getOrderApi = baseUrl +"/v1/regist";
   static const String checkSystemStatusApi = baseUrl +"/v1/regist";
   static const String checkUserStatusApi = baseUrl +"/v1/regist";
+  static const String modifyPasswordApi = baseUrl +"/v1/regist";
 
 
 }
